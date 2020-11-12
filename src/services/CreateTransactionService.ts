1911263bf08df72c0f36d63d3d0c0b4afbf6f916
Paulo Teodoro <paulo-teodoro@MacBook-Air-de-Paulo.local>
2020-11-12 00:52:59 -0300

    desafios conpletos , todos passando pelos testes propostos

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: CreateDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error(
+        'The outcome amount cannot be greater than availabre balance',
+      );
+    }
+    const transation = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transation;
   }
 }
 
